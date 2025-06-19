f1c09529b9be799822c7579937e1a01d0990bb23
Alberto Robledo <robledo.mdp16@gmail.com>
2025-06-19 11:34:30 -0300

    nueva metadata y favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,28 @@ export const metadata: Metadata = {
   keywords:
     "clases de inglés, clases de español, formación lingüística, inglés para empresas, español para empresas, cursos de inglés online, cursos de español online, clases de idiomas, formación empresarial",
   authors: [{name: "Connect Languages"}],
+  icons: {
+    icon: "/logo.webp",
+    apple: "/logo.webp",
+  },
+  openGraph: {
+    title:
+      "Domina el Español e Inglés con Connect Languages - Formación para Empresas y Profesionales",
+    description:
+      "Clases de inglés y español para empresas y profesionales. Aprende con un método comunicativo y horarios flexibles. Desde A1 hasta C2.",
+    url: "https://www.connectlanguages.com.ar",
+    siteName: "Connect Languages",
+    images: [
+      {
+        url: "https://www.connectlanguages.com.ar/og-image.webp",
+        width: 1200,
+        height: 630,
+        alt: "Connect Languages - Formación en Inglés y Español",
+      },
+    ],
+    locale: "es_ES",
+    type: "website",
+  },
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
